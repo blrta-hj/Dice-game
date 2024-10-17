9db85d0d11747bc252e2e3cc200dcc32435b1307
Blerta <95289985+blrta-hj@users.noreply.github.com>
2024-10-17 09:00:20 +0200

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,22 @@
+function diceGame(){
+    var randomNumber1 = Math.floor(Math.random() * 6) +1;
+    var randomNumber2 = Math.floor(Math.random() * 6) +1;
+    //Left dice
+    var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+    var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+    //Right dice
+    var randomDiceImage = "images/dice" + randomNumber2 + ".png";
+    var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage);
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML ="Player 1 Wins 🚩";
+    } else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML ="Player 2 Wins 🚩";
+
+    } else{
+        document.querySelector("h1").innerHTML ="Draw! ";
+
+    }
+}
+
